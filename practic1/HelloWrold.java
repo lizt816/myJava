19691f8e06102b68fdab47102040eb991446d1dd
lizt816 <1579436877@qq.com>
2023-09-07 10:17:00 +0800

    init

---
+++ practic1/HelloWrold.java
@@ -0,0 +1,7 @@
+class HelloWrold{
+   public static void main(String[] args){
+     int age = 12;
+     char gender = '女';
+     System.out.println("hello world"+age+gender);
+   }
+}
---
+++ practic1/VariableTest.java
@@ -0,0 +1,53 @@
+class VariableTest {
+ public static void main(String[] args){
+
+//   *** 命名规范
+// ** 包名：多单词组成时，所有单词的首字母都小写
+// ** 接口名：多单词组成首字母都是大写
+// ** 变量名：多单词组成时首字母小写，后续组成单词首字母大写
+// ** 产量名：所有字母都是大写，多当成用下划线隔开DSA_DAS
+//   *** 定义变量： 
+//   ** 中文如果不能被javac MyJava.java  这样识别，就使用其他命令：
+//     * javac -encoding UTF-8 HelloWrold.java
+// ** 基本数据类型：8种
+//  整型：byte \ short \ int \ long
+//  浮点型：float \ doubl
+//  字符型：char
+//  布尔型：boolean
+
+// ** 引用数据类型：
+//  类（ class ）
+//  数组 （ array ）
+//  接口 （ interface ）
+//  枚举 （ enum ）
+//  注解 （ annotation ）
+//  记录 （ record ）
+
+//    次方就是2*n次方次；
+//  * byte age = 127； byte( 字节 ) 的最大值是127 
+//    * 占位：1byte = 8bit位，-128 ~ 127
+//  * short myShort = 12 最大值为 -2的15次方 ~ 2的15次方-1    
+//    * 1short = 2字节   8bit * 2字节 = 16位
+//  * int age = 12;  最大值为 -2的31次方 ~ 2的31次方-1 ( 约21亿 )  
+//    * 1int = 4字节 ； 8bit * 4字节 = 32位
+//  * long myLong = 12；最大值为 -2的63次方 ~ 2的63次方-1     
+//    * 1long =8字节 ； 8bit * 8字节 = 64位
+//    在声明long变量时，值要求在末尾添加大写或者小写的L
+// ** 他们的关系为：
+//   * 8bit = 1byte
+//   * 1024byte = 1KB
+//   * 1024KB = 1MB
+//   * 1024MB = 1GB
+//   * 1024GB = 1TB
+
+//  * char gender = '男'; // 性别 char 字符串类型的变量
+//  * int // 数字类型
+//  * char // 定义单个字符串的类型
+//  * float // 定义浮点类型
+//  * boolean // 布尔类型
+//  * 
+
+
+
+ }
+}
