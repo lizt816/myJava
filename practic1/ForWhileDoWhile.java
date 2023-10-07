e31770b12905c849a81ec3614c936d0662f855a3
lizt816 <1579436877@qq.com>
2023-10-07 18:02:04 +0800

    javaaaa

---
+++ practic1/ForWhileDoWhile.java
@@ -0,0 +1,79 @@
+/**
+    * 主讲解循环结构
+   */
+class ForWhileDoWhile{
+   public static void main(String[] args){
+     // for(int i = 0; i < 5;i++){
+     //  System.out.println(i);
+     // }
+
+     // int i = 0;
+     // while (i < 50) {
+     //   System.out.println(i);
+     //   i++;
+     // }
+     
+     // 先执行一遍在进行判断在执行
+     // int i = 0;   //
+     // do{
+     //  System.out.println(i);
+     //  i++;
+     // }while(i < 3);
+
+     // System.out.println('i');
+
+
+
+     // 外循环制行，内循环控制列，打印星星
+     /*
+      * * * * * * *  
+       * * * * * *
+        * * * * *
+         * * * *
+           * *
+     */
+
+     // for (int i = 1; i <= 4; i++) {
+     //  for (int j = 1; j <= 2 * i ; j++) {
+     //   System.out.print(' ');
+     //  }
+     //  for (int k = 0; k <= 9-2*i; k++) {
+     //   System.out.print("* ");
+     //  }
+     //  System.out.println();
+     // }
+     // 九九乘法表
+     // for (int i = 1; i <= 9; i++) {
+     //  for (int j = 1; i >= j ; j++) {
+     //     System.out.print(j+"*"+i+"="+i*j+"\t");
+     //  }
+     //  System.out.println();
+     // }
+
+     // break 和 continue 
+     /**
+      * break 是结束当前的循环
+      * continue 是马上进入下一次循环
+      * 
+     */
+
+      // 质数 ：只能被1和他本身整除的自然数,也叫因数,如 3 不能被整除 ，4 可以被整除 因为2*2就是4
+      for (int i = 2; i <= 100; i++) {
+       boolean isFlag = true;
+       for (int j = 2; j < i; j++) {
+        if(i % j == 0){
+         isFlag = false;
+        }
+       }
+       if(isFlag){
+        System.out.println(i);
+       }
+      }
+
+
+      // 优化
+
+
+
+   }
+}
--- practic1/ScannerTest.java
+++ practic1/ScannerTest.java
@@ -1,3 +1,4 @@
+
 /*
   * 如何从键盘获取到不同类型 如：基本数据类型，String类型 的变量：使用Scanner类
   * 1. 使用Scanner获取不同类型数据的步骤
@@ -16,37 +17,58 @@
   *   * 获取到char类型不能获取，只能用其他方法： char gender = scan.next().charAt(0);  获取字符串的第一个下标
   *   * Scanner只能获取到 byte、short、int、long、float、double、boolean、String 类型变量的方法;
   */
+import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 class ScannerTest {
-  public static void main(String[] args){
-   Scanner scanner = new Scanner(System.in);
-   System.out.println("请输入姓名为");
-   String name = scanner.next();
-   callback(name,scanner);
-   System.out.println("请输入年龄");
-   callbackInt(scanner);
-   System.out.println("哇塞");
-   scanner.close();
+ public static void main(String[] args) {
+  Scanner scanner = new Scanner(System.in);
+  System.out.println("请输入姓名为");
+  String name = scanner.next();
+  callback(name, scanner);
+  System.out.println("请输入年龄");
+  callbackInt(scanner);
+  System.out.println("哇塞");
+  scanner.close();
+ }
+
+ public static void callback(String name, Scanner scanner) {
+  System.out.println("你输入的名字为: " + name);
+  if (name.equals("涛哥") || name != "") {
+   System.out.println("ok!");
+  } else {
+   System.out.println("no!");
+   String names = scanner.next();
+   callback(names, scanner);
   }
-  public static void callback(String name,Scanner scanner){
-   System.out.println("你输入的名字为: " + name);
-   if(name.equals("涛哥") || name != ""){
-    System.out.println("ok!");
-   } else{
-    System.out.println("no!");
-    String names = scanner.next();
-    callback(names,scanner);
-   }
-  }
-  public static void callbackInt(Scanner scanner){
+ }
+
+ public static void callbackInt(Scanner scanner) {
+  // 无限循环while (true)，使用break可以跳出循环，
+  // 如果执行到了catch后使用System.err.println("输入的年龄不是有效的整数，请重新输入: ");
+  // 进行提示然后在继续try
+  while (true) {
    try {
     int age = scanner.nextInt();
     System.out.println("你输入的年龄为: " + age);
     System.out.println("ok!");
-   } catch (Exception e) {
-    System.out.println(e);
-    callbackInt(scanner);
+    break; // 输入正确，跳出循环
+   } catch (InputMismatchException e) {
+    System.err.println("输入的年龄不是有效的整数，请重新输入: ");
+    scanner.next(); // 清除输入缓冲区
    }
   }
+
+   // 使用递归的写法
+   // try {
+   //  int age = scanner.nextInt();
+   //  System.out.println("你输入的年龄为: " + age);
+   //  System.out.println("ok!");
+   // } catch (InputMismatchException e) {
+   //     scanner.next();   // 清除输入的缓存区
+   //     System.err.println("输入的年龄不是有效的整数: " + e.getMessage());
+   //     callbackInt(scanner);
+   // }
+ }
 }
