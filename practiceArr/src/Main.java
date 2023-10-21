03023e58b4fdc7a3d9c8f763b4fb76b02b845d05
lizt816 <1579436877@qq.com>
2023-10-21 20:31:33 +0800

    数组的栈和堆

--- practiceArr/src/Main.java
+++ practiceArr/src/Main.java
@@ -9,6 +9,33 @@
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+//        声明浮点
+        double[] proces = new double[]{12.0,18.1,99,1234.55};
+        for (int i = 0; i < proces.length; i++) {
+            System.out.println("proces="+proces[i]);
+        }
+
+//        声明数字
+        int[] arr = new int[]{12,45,12};
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("int="+arr[i]);
+        }
+
+//        声明字符串
+        String[] foods = new String[]{"张三","李四"};
+        for (int i = 0; i < foods.length; i++) {
+            System.out.println("String="+foods[i]);
+        }
+
+
+//        栈和堆
+    int[] arrs = new int[3];
+
+        arrs[0] = 10;
+      int[] arr2 = arrs;
+        arr2[0] = 20;
+        System.out.println(arrs[0]+"arrr");
+
     }
 }
